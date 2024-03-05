85864e529b327b4c0abab13b26e510a4b27b1f6a
Florencia Morisse <florenciamorisse@gmail.com>
2024-03-05 08:52:41 -0300

    conecté los links del menú desplegable

--- script.js
+++ script.js
@@ -4,12 +4,37 @@ let bar1 = document.getElementById("line1");
 let bar2 = document.getElementById("line2");
 let bar3 = document.getElementById("line3");
 let menu_desplegable= document.getElementById("menu_desplegable");
+let btn_inicio = document.getElementById("btn_inicio");
+let btn_servicios = document.getElementById("btn_servicios");
+let btn_contacto = document.getElementById("btn_contacto");
 
 bar_menu.addEventListener("click", function animateBars(){
     bar1.classList.toggle("closeline1");
     bar2.classList.toggle("closeline2");
     bar3.classList.toggle("closeline3");
     menu_desplegable.classList.toggle("show")
+})
+
+btn_inicio.addEventListener("click", function close_menu_desplegable(){
+    menu_desplegable.classList.toggle("show");
+    bar1.classList.toggle("closeline1");
+    bar2.classList.toggle("closeline2");
+    bar3.classList.toggle("closeline3");
+})
+
+
+btn_servicios.addEventListener("click", function close_menu_desplegable(){
+    menu_desplegable.classList.toggle("show");
+    bar1.classList.toggle("closeline1");
+    bar2.classList.toggle("closeline2");
+    bar3.classList.toggle("closeline3");
+})
+
+btn_contacto.addEventListener("click", function close_menu_desplegable(){
+    menu_desplegable.classList.toggle("show");
+    bar1.classList.toggle("closeline1");
+    bar2.classList.toggle("closeline2");
+    bar3.classList.toggle("closeline3");
 })
 
 
