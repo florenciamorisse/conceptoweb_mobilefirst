df588ad42e65c3afeaa98f4842ed15f75a52ed0d
Florencia Morisse <florenciamorisse@gmail.com>
2024-04-14 18:33:19 -0300

    msj confirmacion

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ let menu_desplegable= document.getElementById("menu_desplegable");
 let btn_inicio = document.getElementById("btn_inicio");
 let btn_servicios = document.getElementById("btn_servicios");
 let btn_contacto = document.getElementById("btn_contacto");
+let msj_confirmacion = document.getElementById("msj_confirmacion");
+let btn_enviar = document.getElementById("enviar");
 // let btn_abrirModal= document.getElementById("enviar");
 // let btn_cerrarModal= document.getElementById("btn_cerrarModal");
 // let modal= document.getElementById("modal");
@@ -38,6 +40,10 @@ btn_contacto.addEventListener("click", function close_menu_desplegable(){
     bar1.classList.toggle("closeline1");
     bar2.classList.toggle("closeline2");
     bar3.classList.toggle("closeline3");
+})
+
+btn_enviar.addEventListener("click", function mensaje(){
+    msj_confirmacion.classList.toggle("oculto");
 })
 
 // btn_abrirModal.addEventListener("click",()=>{
